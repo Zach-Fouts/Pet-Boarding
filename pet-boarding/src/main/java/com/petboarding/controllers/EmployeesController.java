5c1dd06de6f4c981da4e51b457c7ef704e996226
Ulises Peguero <peguero.ulises@gmail.com>
2023-01-24 02:21:11 -0600

    Created grid view for employees and fixed CSS detail with the module name.

---
+++ pet-boarding/src/main/java/com/petboarding/controllers/EmployeesController.java
@@ -0,0 +1,36 @@
+package com.petboarding.controllers;
+
+import com.petboarding.models.Employee;
+import com.petboarding.models.data.EmployeeRepository;
+import com.petboarding.models.data.PositionRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("employees")
+public class EmployeesController extends AppBaseController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @Autowired
+    private PositionRepository positionRepository;
+
+
+    @GetMapping
+    public String displayEmployeesGrid(Model model) {
+        setActiveModule("employees", model);
+        model.addAttribute("employees", employeeRepository.findAll());
+        return "employees/index";
+    }
+
+    @GetMapping("add")
+    public String displayAddEmployeeForm(Model model) {
+        setActiveModule("employees", model);
+        model.addAttribute("employee", new Employee());
+        return "employees/add";
+    }
+}
--- pet-boarding/src/main/java/com/petboarding/models/Employee.java
+++ pet-boarding/src/main/java/com/petboarding/models/Employee.java
@@ -61,6 +61,10 @@ public class Employee extends AbstractEntity{
         this.lastName = lastName;
     }
 
+    public String getFullName() {
+        return this.lastName + ", " + this.firstName;
+    }
+
     public Position getPosition() {
         return position;
     }
