45d5f77e35156a2f54484b39cff66bb9b7435924
Tigioz <z.fouts@icloud.com>
2023-01-28 10:59:57 -0600

    added role CRUD, fixed styling in user pages

--- pet-boarding/src/main/java/com/petboarding/controllers/UserManagementController.java
+++ pet-boarding/src/main/java/com/petboarding/controllers/UserManagementController.java
@@ -1,9 +1,11 @@
 package com.petboarding.controllers;
 
 import com.petboarding.models.Role;
+import com.petboarding.models.app.Module;
 import com.petboarding.models.data.RoleRepository;
 import com.petboarding.models.data.UserRepository;
 import com.petboarding.models.dto.AddNewUserDTO;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -14,17 +16,18 @@ import com.petboarding.models.User;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
 import java.util.*;
+import java.util.stream.Collectors;
 
 @Controller
 @RequestMapping("users")
-public class UserManagementController {
+public class UserManagementController extends AppBaseController{
     @Autowired
     private UserRepository userRepository;
     @Autowired
     private RoleRepository roleRepository;
 
 
-
+// -----------------------------User Management------------------------------
     @GetMapping("/addUserForm")
     public String addUserForm(Model model) {
         List<Role> listRoles = (List<Role>) roleRepository.findAll();
@@ -38,6 +41,9 @@ public class UserManagementController {
 
         if (errors.hasErrors()) {
             model.addAttribute("title", "Add User");
+            List<Role> listRoles = (List<Role>) roleRepository.findAll();
+            model.addAttribute("listRoles", listRoles);
+
             return "/users/addUserForm";
         }
 
@@ -46,7 +52,10 @@ public class UserManagementController {
         if (existingUser != null) {
             errors.rejectValue("username", "username.alreadyexists", "A user with that username already exists");
             model.addAttribute("title", "Add User");
+            List<Role> listRoles = (List<Role>) roleRepository.findAll();
+            model.addAttribute("listRoles", listRoles);
             return "/users/addUserForm";
+
         }
 
         String password = addNewUserDTO.getPassword();
@@ -54,16 +63,19 @@ public class UserManagementController {
         if (!password.equals(verifyPassword)) {
             errors.rejectValue("password", "passwords.mismatch", "Passwords do not match");
             model.addAttribute("title", "Register");
+            List<Role> listRoles = (List<Role>) roleRepository.findAll();
+            model.addAttribute("listRoles", listRoles);
             return "/users/addUserForm";
         }
 
         User newUser = new User(addNewUserDTO.getUsername(), addNewUserDTO.getPassword(), addNewUserDTO.getRole());
+        addNewUserDTO.getRole().users.add(newUser);
         userRepository.save(newUser);
 
-        return "redirect:/users/userList";
+        return "redirect:/users";
     }
 
-    @GetMapping("/userList")
+    @GetMapping("")
     public String listUsers(Model model) {
         List<User> listUsers = (List<User>) userRepository.findAll();
         model.addAttribute("listUsers", listUsers);
@@ -90,8 +102,9 @@ public class UserManagementController {
         return "/users/editUserForm";
     }
 
+    //Saves user profile changes in editUserForm
     @Transactional
-    @PostMapping("/save")
+    @PostMapping("/saveUser")
     public String submitSaveUser(@RequestParam int id,
                                  @RequestParam String username,
                                  @RequestParam Role role,
@@ -103,7 +116,7 @@ public class UserManagementController {
             user.setRole(role);
             userRepository.save(user);
         }
-        return "redirect:/users/userList";
+        return "redirect:/users";
     }
 
     @GetMapping("/deleteUserForm/{id}")
@@ -117,15 +130,70 @@ public class UserManagementController {
         return "users/deleteUserForm";
     }
 
-    @PostMapping("/delete")
+    //Deletes user in deleteUserForm
+    @PostMapping("/deleteUser")
     public String deleteUser(@RequestParam(required = true) int id) {
         Optional<User> user = userRepository.findById(id);
         if(user.isPresent()){
         userRepository.delete(user.get());
         }
-
-
-        return "redirect:/users/userList";
+        return "redirect:/users";
     }
 
+
+    // -----------------------------Role Management------------------------------
+
+    @GetMapping("roles/roleList")
+    public String roleList(Model model){
+        List<Role> listRoles = roleRepository.findAll();
+        model.addAttribute("listRoles", listRoles);
+        return "/users/roles/roleList";
+    }
+
+    @GetMapping("/roles/addRoleForm")
+    public String addRoleForm(Model model) {
+        model.addAttribute("role", new Role());
+        return "/users/roles/addRoleForm";
+    }
+
+    //Saves role in addRoleForm
+    @PostMapping("roles/saveRole")
+    public String createRole(@RequestParam String name)  {
+        Role newRole = new Role(name);
+        roleRepository.save(newRole);
+        return "redirect:/users/roles/roleList";
+    }
+
+    @GetMapping("roles/deleteRoleForm/{id}")
+    public String deleteRoleForm(@PathVariable("id") int id, Model model){
+        Optional<Role> role = roleRepository.findById(id);
+        if (role.isPresent()) {
+            model.addAttribute("role", role.get());
+        }
+
+        return "users/roles/deleteRoleForm";
+    }
+
+    @PostMapping("roles/deleteRoleForm")
+    public String deleteRole(@RequestParam int id, Model model) {
+            Role role = roleRepository.findById(id).get();
+
+        List<User> users = role.getUsers();
+        List<User> filteredUsers = users.stream().filter(user -> user.getRole().getId() == id).collect(Collectors.toList());
+            if (!filteredUsers.isEmpty()) {
+            model.addAttribute("errorMessage", "This role is currently assigned to one or more users and cannot be deleted.");
+            model.addAttribute("role", role);
+
+            return "/users/roles/deleteRoleForm";
+        }
+            else {
+            roleRepository.deleteById(role.getId());
+            return "redirect:/users/roles/roleList";
+        }
+    }
+
+    @ModelAttribute("activeModule")
+    public Module addActiveModule() {
+        return getActiveModule("users");
+    }
 }
--- pet-boarding/src/main/java/com/petboarding/models/Role.java
+++ pet-boarding/src/main/java/com/petboarding/models/Role.java
@@ -3,16 +3,19 @@ package com.petboarding.models;
 
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
 import java.util.ArrayList;
 import java.util.List;
 
 @Entity
 public class Role extends AbstractEntity{
     @OneToMany
-    private List<User> users = new ArrayList<>();
+    public List<User> users = new ArrayList<>();
 
+    @NotBlank
+    @NotNull
     private String name;
-
 
     public Role() {}
 
