8bebaa22c456cc4e916b5ef1e01a7e4609bda047
Ulises Peguero <peguero.ulises@gmail.com>
2023-01-28 16:19:43 -0600

    Completed job positions

--- pet-boarding/src/main/resources/static/js/uiUtils.js
+++ pet-boarding/src/main/resources/static/js/uiUtils.js
@@ -2,9 +2,13 @@ let deleteDialog;
 
 // Updates the name of the employee's record that is going to be tried to delete,
 // also updates the submit route for the controller
-// @param name  {string}
-// @param id    {number}
-function openDeleteDialog(name, id, path = '/employees') {
+// @param name  {string}    Name/Description that represent the entry that wants to be deleted.  "Employee: Name LastName"
+// @param path  {string}    Base path of the @RequestMapping for the controller that will handle the delete request.
+//                          "/employees" -> "/employees/delete/{id}"
+// @param id    {number}    Id of the entry we are trying to delete, it will be added to the end of the path.
+//
+// Thymeleaf:   <a type="button" clas="btn btn-danger" th:href="|javascript:openDeleteDialog('${model.name}', '/employees/positions', ${model.id});|">Delete</a>
+function openDeleteDialog(name, path, id) {
     addDeleteDialogElement();
     document.getElementById('deleteName').textContent = name;
     deleteForm.action = `${path}/delete/${id}`;
@@ -22,11 +26,11 @@ function addDeleteDialogElement( ) {
                     <div class="modal-dialog modal-dialog-centered">
                         <div class="modal-content">
                             <div class="modal-header">
-                                <h5 class="modal-title">Delete employee</h5>
+                                <h5 class="modal-title">Delete entry</h5>
                                 <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                             </div>
                             <div class="modal-body">
-                                <p>Do you want to delete <span id="deleteName" class="fw-semibold"></span>'s entry?. To continue press delete.</p>
+                                <p>Do you want to delete <span id="deleteName" class="fw-semibold"></span>?. To continue press delete.</p>
                             </div>
                             <div class="modal-footer">
                                 <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancel</button>
@@ -38,7 +42,13 @@ function addDeleteDialogElement( ) {
                     </div>
                 </div>
         `);
-        document.querySelector('main.content').appendChild(dialogFragment);
+        let container = document.querySelector('main.content');
+        if(!container) container = document.body; // not using layout
+        try{
+            container.appendChild(dialogFragment);
+        } catch(exception) {
+            document.alert("An error has occurred while trying to create the modal Dialog.");
+        }
     }
     deleteDialog = new bootstrap.Modal('#deleteDialog', {});
 }
