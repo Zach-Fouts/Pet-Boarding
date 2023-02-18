6054cd17bee2b81783b14c0b71eb3330650edf23
Ulises Peguero <peguero.ulises@gmail.com>
2023-02-18 00:03:11 -0600

    Added catalogues to stay form

--- pet-boarding/src/main/java/com/petboarding/controllers/StayController.java
+++ pet-boarding/src/main/java/com/petboarding/controllers/StayController.java
@@ -5,15 +5,19 @@ import com.petboarding.models.Reservation;
 import com.petboarding.models.Stay;
 import com.petboarding.models.StayStatus;
 import com.petboarding.models.app.Module;
+import com.petboarding.models.data.EmployeeRepository;
 import com.petboarding.models.data.ReservationRepository;
 import com.petboarding.models.data.StayRepository;
 import com.petboarding.models.data.StayStatusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.validation.Valid;
+import java.util.Date;
 import java.util.Optional;
 
 @Controller
@@ -28,6 +32,9 @@ public class StayController extends AppBaseController {
 
     @Autowired
     private ReservationRepository reservationRepository;
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
 
     @Autowired
     private StayStatusRepository stayStatusRepository;
@@ -50,8 +57,15 @@ public class StayController extends AppBaseController {
         return "stays/form";
     }
 
+    @PostMapping("add")
+    public String proccessAddStayForm(@RequestParam(required = false) String endDate, Model model) {
+        System.out.println(endDate);
+        return "redirect:/add";
+    }
+
     private void prepareCommonFormModel(Model model) {
-        model.addAttribute("status", stayStatusRepository.findAll());
+        model.addAttribute("statuses", stayStatusRepository.findAll());
+        model.addAttribute("caretakers", employeeRepository.findByPositionName("caretaker"));
     }
     private void prepareAddFormModel(Stay stay, Model model) {
         model.addAttribute("formTitle", FORM_NEW_TITLE);
