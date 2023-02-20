52153539862933237c62cc1c217863fb54e04d93
Ulises Peguero <peguero.ulises@gmail.com>
2023-02-19 20:22:16 -0600

    Added model and repository for Services

---
+++ pet-boarding/src/main/java/com/petboarding/models/data/ServiceRepository.java
@@ -0,0 +1,11 @@
+package com.petboarding.models.data;
+
+import com.petboarding.models.Service;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Collection;
+
+public interface ServiceRepository extends JpaRepository<Service, Integer> {
+    public Collection<Service> findByStayService(Boolean isStayService);
+    )
+}
