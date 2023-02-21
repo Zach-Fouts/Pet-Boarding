ceb09923b49419751d9e51af9483b103bde15729
Ulises Fox Peguero <peguero.ulises@gmail.com>
2023-02-20 18:58:46 -0600

    Revert "Kennel map investigation"

--- pet-boarding/src/main/java/com/petboarding/models/Kennel.java
+++ pet-boarding/src/main/java/com/petboarding/models/Kennel.java
@@ -1,13 +1,11 @@
 package com.petboarding.models;
 
-
 import javax.persistence.Entity;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
 @Entity
 public class Kennel extends AbstractEntity{
-
 
     @NotNull
     @Size(max = 25, message = "The name cannot be longer than 50 characters.")
@@ -20,4 +18,7 @@ public class Kennel extends AbstractEntity{
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
 }
