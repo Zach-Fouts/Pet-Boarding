4ab54f52afaa99a276c94ea7816fd09da55b5b7d
Ulises Peguero <peguero.ulises@gmail.com>
2023-03-01 03:24:45 -0600

    Removed unnecesary code from stripePayment.js

--- pet-boarding/src/main/resources/static/js/stripePayment.js
+++ pet-boarding/src/main/resources/static/js/stripePayment.js
@@ -7,7 +7,6 @@ let stripeElements;
 let elements;
 
 loadPaymentForm();
-checkStatus();
 
 $('#payment-form').submit(handleSubmit);
 
@@ -46,34 +45,6 @@ async function handleSubmit(e) {
   }
 
   setLoading(false);
-}
-
-// Fetches the payment intent status after payment submission
-async function checkStatus() {
-  const clientSecret = new URLSearchParams(window.location.search).get(
-    'payment_intent_client_secret'
-  );
-
-  if (!clientSecret) {
-    return;
-  }
-
-  const { paymentIntent } = await stripe.retrievePaymentIntent(clientSecret);
-
-  switch (paymentIntent.status) {
-    case 'succeeded':
-      showMessage('Payment succeeded!');
-      break;
-    case 'processing':
-      showMessage('Your payment is processing.');
-      break;
-    case 'requires_payment_method':
-      showMessage('Your payment was not successful, please try again.');
-      break;
-    default:
-      showMessage('Something went wrong.');
-      break;
-  }
 }
 
 function displayErrorAlert(message) {
